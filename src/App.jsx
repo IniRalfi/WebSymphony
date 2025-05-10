757f1044995a18f716c7844515cba3db0690e51a
IniRalfi <rflipratm@gmail.com>
2025-05-10 22:50:27 +0700

    hapus komen di app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CircleGallery from '/components/CircleGallery/CircleGallery';
-import AboutSymphony from '/components/AboutSymphony/AboutSymphony'; // pastikan sudah di-import
+import AboutSymphony from '/components/AboutSymphony/AboutSymphony';
 
 function App() {
   return (
