a4d1453095f10605922c42936d9359f7899757e8
syidmen <rasyidmuh0907@gmail.com>
2025-05-11 17:32:00 +0700

    headerfooter + asset

---
+++ src/pages/Lagu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CardGallery from "../components/Card/CardGallery";
+
+const Lagu = () => {
+  return (
+    <div>
+      <CardGallery />
+    </div>
+  );
+};
+
+export default Lagu;
